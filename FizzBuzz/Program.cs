bf81f25b9fba94dc8c377d2386cd5d0dfb760361
lukeott11 <54411134+lukeott11@users.noreply.github.com>
2019-09-15 21:57:53 -0500

    Add files via upload

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,56 @@
+﻿// Luke Ott
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string luckynum;
+            int number;
+            string output = "";
+            
+            Console.WriteLine("Please input your lucky number");
+            luckynum = Console.ReadLine();
+           
+            bool verify = Int32.TryParse(luckynum, out number);
+            if (verify == true)
+            {
+               
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    output = "FizzBuzz";
+                }
+                else if (number % 5 == 0)
+                {
+                    output = "Buzz";
+                }
+                else if (number % 3 == 0)
+                {
+                    output = "Fizz";
+                }
+                else
+                {
+                  
+                    output = Convert.ToString(number);
+                }
+
+            }
+            else
+            {
+                
+                Console.WriteLine("Sorry, I am unable to run the FizzBuzz process because the number you inputted is not a valid");
+                Console.ReadKey();
+            }
+            
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
